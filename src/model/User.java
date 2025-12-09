3ddc04fc0b54866218ae042c5bb57d18e9224f22
Javier Martín Rodríguez <132887091+JaviMRO@users.noreply.github.com>
2025-12-09 17:57:32 +0000

    Change User class to abstract and add constructor
    Class User created

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,27 @@
 package model;
 
-public class User {
+public abstract class User {
+    private String id;
+    private String name;
+    private String email;
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + email + ")";
+    }
+
 }
