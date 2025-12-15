64eb78d5052292a18e0fe2a9459b4c7b50759a4b
Tomás Santana Suárez <ts.suarez2006@gmail.com>
2025-12-15 11:26:16 +0000

    Add Javadoc comments to TaxReturnEstatus
    Added documentation for the TaxReturnEstatus enum.

--- src/model/taxReturn/TaxReturnEstatus.java
+++ src/model/taxReturn/TaxReturnEstatus.java
@@ -1,5 +1,12 @@
 package model.taxReturn;
 
+/**
+ * Enumeration representing the lifecycle states of a Tax Return.
+ * <p>
+ * Tracks the progress of the return from creation (DRAFT) to calculation (PROCESSING)
+ * and final submission (FILED).
+ * </p>
+ */
 public enum TaxReturnEstatus {
     DRAFT,
     PROCESSING,
