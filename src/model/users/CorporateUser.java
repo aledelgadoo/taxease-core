ec751650b09a94e8df4bb5693a25e497f5af63a1
Tomás Santana Suárez <ts.suarez2006@gmail.com>
2025-12-15 11:21:26 +0000

    Document CorporateUser class with Javadoc
    Added documentation for CorporateUser class.

--- src/model/users/CorporateUser.java
+++ src/model/users/CorporateUser.java
@@ -1,5 +1,13 @@
 package model.users;
 
+/**
+ * Represents a corporate client or SME (B2B).
+ * <p>
+ * This class extends the generic {@link User} to include business-specific details,
+ * such as the Company Registration Number. It corresponds to the "TaxEase Pro" module
+ * [cite_start]designed for SMEs[cite: 15, 44].
+ * </p>
+ */
 public class CorporateUser extends User {
     private String companyRegNumber; // CIF or Registration Number
 
