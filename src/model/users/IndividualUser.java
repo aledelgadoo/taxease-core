5f69ee37f26dd77deb7cbac5ba4438ac2dbaebfa
Tomás Santana Suárez <ts.suarez2006@gmail.com>
2025-12-15 11:20:27 +0000

    Add documentation for IndividualUser class
    Added documentation for IndividualUser class explaining its purpose and relation to the User class.

--- src/model/users/IndividualUser.java
+++ src/model/users/IndividualUser.java
@@ -1,5 +1,13 @@
 package model.users;
 
+/**
+ * Represents an individual taxpayer (B2C client).
+ * <p>
+ * This class extends the generic {@link User} to include specific identification
+ * for natural persons, such as the personal Tax ID (e.g., DNI or Social Security Number).
+ * [cite_start]It corresponds to the "TaxEase Personal" module described in the project dossier[cite: 15, 42].
+ * </p>
+ */
 public class IndividualUser extends User {
     private String taxId;
 
