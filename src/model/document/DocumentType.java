7a6bacdbad1eb6eb03ac4a8dc3b0edb8067e70c0
Tomás Santana Suárez <ts.suarez2006@gmail.com>
2025-12-15 11:23:49 +0000

    Add documentation for DocumentType enumeration
    Added documentation comments to the DocumentType enum.

--- src/model/document/DocumentType.java
+++ src/model/document/DocumentType.java
@@ -1,5 +1,12 @@
 package model.document;
 
+/**
+ * Enumeration defining the specific types of documents the system can recognize.
+ * <p>
+ * Used to categorize documents as either income (e.g., PAYSLIP) or deductible
+ * [cite_start]expenses (e.g., INVOICE, RECEIPT) for the deduction engine[cite: 50].
+ * </p>
+ */
 public enum DocumentType {
     INVOICE,
     PAYSLIP,
